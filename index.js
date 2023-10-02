28dd117d0fa48be364bdf3fec7d237cf66b059eb
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-02 07:03:19 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun()); 
+console.log(saturdayFun("go hiking"));
+
+const mondayWork = function(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork()); 
+console.log(mondayWork('work from home')); 
+
+function wrapAdjective(highlight = '*') {
+  return function(adjective) {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
+const starWrapper = wrapAdjective('*');
+const doublePipeWrapper = wrapAdjective('||');
+
+console.log(starWrapper('awesome'));
+console.log(doublePipeWrapper('fantastic'));
